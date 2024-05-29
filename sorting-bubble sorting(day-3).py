fff4af3a0a3412de51a4aad41d1d3c9354d1b210
rajugovvala <138850572+rajugovvala@users.noreply.github.com>
2024-05-29 11:45:12 +0530

    Add files via upload

---
+++ sorting-bubble sorting(day-3).py
@@ -0,0 +1,20 @@
+def performbubblesort(nums):
+    n=len(nums)
+    fixthisindex=n-1
+    while fixthisindex > 0:
+        for index in range(fixthisindex):
+            if nums[index]>nums[index+1]:
+                temp=nums[index]
+                nums[index]=nums[index+1]
+                nums[index+1]=temp
+        print(nums)
+        fixthisindex -=1
+
+#nums=[10,8,2,14,12,85]
+nums=[list(map(int,input().split()))]
+print("before sorting",nums)
+
+performbubblesort(nums)
+print("after sorting:",nums)
+            
+            
